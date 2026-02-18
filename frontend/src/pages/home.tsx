786aedb81ab380e64197f12248bc7de321a6a46b
luca-avila <avilaluca61@gmail.com>
2026-02-18 14:44:11 -0300

    Implement homepage

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import CounterDisplay from '../components/counterDisplay';
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Counter</h1>
+      <CounterDisplay />
+    </main>
+  );
+};
+
+export default Home;
