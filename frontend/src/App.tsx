858b50b730919074492ffaab2c40dbf90cddf5ed
luca-avila <avilaluca61@gmail.com>
2026-02-18 14:00:18 -0300

    Initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return 0;
+}
+
+export default App
